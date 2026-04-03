1081912a01517e0229ec3ffc017a7e59fadca682
Niklas Wagner <Skaro@Skaronator.com>
2026-04-03 14:11:11 +0200

    ci(release): Setup prereleases

---
+++ .github/release.config.mjs
@@ -0,0 +1,32 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+
+const isRelease = process.env.IS_RELEASE === 'true';
+
+export default {
+  debug: true,
+  branches: isRelease ? ['main'] : [{ name: 'main', prerelease: 'rc' }],
+  tagFormat: '${version}',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        discussionCategoryName: 'Announcements',
+        releasedLabels: false,
+      },
+    ],
+  ],
+};
